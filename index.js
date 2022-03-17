360f827d6b651d8442bd090e1eb1d8802329eb0c
Pashimati <Pashimatii@gmail.com>
2022-03-17 20:06:13 +0300

    config server

--- index.js
+++ index.js
@@ -1,57 +1,41 @@
 const {addOrUpdateFileCollection, deleteFileCollection, getFileCollection} = require('./db/db')
-const {getDocs} = require('firebase/firestore/lite')
-const { doc, setDoc, collection, deleteDoc } = require("firebase/firestore");
+const cors = require('cors')
+
 
 const {DOCTORS} = require('./db/tables')
 
 const express = require('express')
-const bodyParser = require('body-parser')
 
+const jsonParser = express.json();
 const app = express()
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-
-    // authorized headers for preflight requests
-    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-
-    app.options('*', (req, res) => {
-        // allowed XHR methods
-        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
-        res.send();
-    });
-});
 
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors())
 
-// app.get('/', async (request, res) => {
-//
-//     addOrUpdateFileCollection(DOCTORS, '5', {
-//         name: 'vasia',
-//         surname: 'puk',
-//         age: 26,
-//     })
-//
-//     res.json({
-//         success: true
-//     })
-// })
 
-app.post('/add-doctor', async (request, res) => {
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, PATCH, PUT, POST, DELETE, OPTIONS");
+    next();  // передаем обработку запроса методу app.post("/postuser"...
+});
+
+
+app.post('/', jsonParser, async (request, res) => {
     const name = request.body.name
     const surname = request.body.surname
     const age = request.body.age
 
-    addOrUpdateFileCollection(DOCTORS, '1000', {
-        name: name,
-        surname: surname,
-        age: age,
+    addOrUpdateFileCollection(DOCTORS, '4444', {
+        name: 'pasha',
+        surname: 'surname',
+        age: 21,
+
     })
+    console.log(request.body)
+    // console.log(name, surname, age)
 
     res.json({
         success: true,
