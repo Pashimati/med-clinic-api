8fe5d5cb1f2541e8013b07af41aae654f8e7935a
Pashimati <Pashimatii@gmail.com>
2022-04-11 17:05:48 +0300

    update users

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,24 +2,24 @@ const express = require('express');
 const router = express.Router();
 const { addOrUpdateFileCollection, deleteFileCollection, getFileCollection, getAllFromCollection } = require('./../db/db')
 const { USERS } = require('./../db/tables')
-//
-// router.get('/get/:id', async (request, res) => {
-//     const id = request.params.id
-//     let status = true;
-//     const doctor = await getFileCollection(DOCTORS, id);
-//
-//     if (!doctor) {
-//         status = false;
-//     }
-//
-//     res.json({
-//         doctor: doctor,
-//         success: status
-//     })
-// })
+
+router.get('/get/:id', async (request, res) => {
+    const id = request.params.id
+    let status = true;
+    const user = await getFileCollection(USERS, id);
+
+    if (!user) {
+        status = false;
+    }
+
+    res.json({
+        user: user,
+        success: status
+    })
+})
 
 router.post('/add', async (request, res) => {
-    const fileName = request.body.data.fileName
+    const fileName = request.body.data.id
     const name = request.body.data.name
     const surname = request.body.data.surname
     const sex = request.body.data.sex
@@ -51,33 +51,39 @@ router.post('/add', async (request, res) => {
     })
 })
 
-// router.post('/update', async (request, res) => {
-//     const id = request.body.data.id
-//     const name = request.body.data.name
-//     const surname = request.body.data.surname
-//     const speciality = request.body.data.speciality
-//
-//     let message = 'doctor has not been updated'
-//     let success = false;
-//
-//     if (id && name && surname && speciality) {
-//         await addOrUpdateFileCollection(DOCTORS, id, {
-//             id: id,
-//             name: name,
-//             surname: surname,
-//             speciality: speciality,
-//         })
-//             .then((status) => {
-//                 message = 'doctor has been updated'
-//                 success = status
-//             })
-//     }
-//
-//     res.json({
-//         success: success,
-//         message: message,
-//     })
-// })
+router.post('/update', async (request, res) => {
+    const fileName = request.body.data.fileName
+    const name = request.body.data.name
+    const surname = request.body.data.surname
+    const sex = request.body.data.sex
+    const age = request.body.data.age
+    const address = request.body.data.address
+    const phone = request.body.data.phone
+
+    let message = 'user has not been created'
+    let success = false;
+
+    if (fileName && name && surname && sex && age && address && phone) {
+        await addOrUpdateFileCollection(USERS, fileName,{
+            name: name,
+            surname: surname,
+            sex: sex,
+            age: age,
+            address: address,
+            phone: phone,
+        })
+            .then((status) => {
+                message = 'user has been created'
+                success = status
+            })
+    }
+
+    res.json({
+        success: success,
+        message: message,
+    })
+})
+
 
 
 router.post('/delete', async (request, res) => {
