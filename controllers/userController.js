be50e05ae2c04d0052f2d964978fe84e01829e70
Pashimati <Pashimatii@gmail.com>
2022-04-23 17:54:52 +0300

    add controll admin/add user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const { firebaseAdmin } = require("../services/firebase-service");
+const { checkIfAdmin } = require("../middlewares/auth-middleware");
 const router = express.Router();
 const { addOrUpdateFileCollection, deleteFileCollection, getFileCollection, getAllFromCollection } = require('./../db/db')
 const { USERS } = require('./../db/tables')
 
 router.get('/get/:id', async (request, res) => {
-    const fileName = request.params.fileName
+    const fileName = request.params.id
     let status = true;
     const user = await getFileCollection(USERS, fileName);
 
@@ -52,6 +54,57 @@ router.post('/add', async (request, res) => {
 })
 
 
+router.post('/admin/add', checkIfAdmin, async (request, res) => {
+    const password = request.body.data.password
+    const email = request.body.data.email
+    const name = request.body.data.name
+    const surname = request.body.data.surname
+    const sex = request.body.data.sex
+    const age = request.body.data.age
+    const address = request.body.data.address
+    const phone = request.body.data.phone
+
+    let message = 'user has not been created'
+    let success = false;
+
+    const user = await firebaseAdmin.auth().createUser({
+        email,
+        password,
+    });
+
+    const uid = user.uid
+    firebaseAdmin.auth()
+        .setCustomUserClaims(uid, { user: true })
+        .then(() => {
+            // The new custom claims will propagate to the user's ID token the
+            // next time a new one is issued.
+        });
+
+    if (name && surname && sex && age && address && phone) {
+        await addOrUpdateFileCollection(USERS, uid,{
+            userUid: uid,
+            name: name,
+            surname: surname,
+            sex: sex,
+            age: age,
+            address: address,
+            phone: phone,
+        })
+            .then((status) => {
+                message = 'user has been created'
+                success = status
+            })
+    }
+
+    res.json({
+        success: success,
+        message: message,
+    })
+})
+
+
+
+
 router.post('/update', async (request, res) => {
     const fileName = request.body.data.fileName
     const name = request.body.data.name
@@ -87,7 +140,7 @@ router.post('/update', async (request, res) => {
 
 
 
-router.post('/delete', async (request, res) => {
+router.post('/admin/delete', checkIfAdmin, async (request, res) => {
     const fileName = request.body.id
 
     let message = 'user has been deleted'
@@ -116,7 +169,7 @@ router.post('/delete', async (request, res) => {
 })
 
 
-router.get('/get-all', async (request, res) => {
+router.get('/admin/get-all', checkIfAdmin, async (request, res) => {
     let users = [];
     let state = true;
     await getAllFromCollection(USERS)
